79d43340380ce508fb5b5a8c598cd8ebd557bdc9
Mohammed Achraf Lebrazi <lebrazi.med.achraf@gmail.com>
2024-07-14 13:32:21 +0200

    adds error handling in renderer.js

--- renderer.js
+++ renderer.js
@@ -6,16 +6,21 @@ import { forwardSelectedOptions } from './renderer/navigation.js';
 document.addEventListener('DOMContentLoaded', async () => {
     console.log('DOM fully loaded and parsed');
 
-    // Check which page is loaded
-    if (window.location.pathname.endsWith('main.html') || window.location.pathname === '/') {
-        await populateDropdownOptions();
-        addRadioEventListeners();
-        addMagicButtonEventListener();
-    }
+    try {
+        // Check which page is loaded
+        if (window.location.pathname.endsWith('main.html') || window.location.pathname === '/') {
+            await populateDropdownOptions();
+            addRadioEventListeners();
+            addMagicButtonEventListener();
+        }
 
-    // Display selected options if on viewer.html
-    if (window.location.pathname.endsWith('viewer.html')) {
-        forwardSelectedOptions();
-        addReloadButtonEventListener();
+        // Display selected options if on viewer.html
+        if (window.location.pathname.endsWith('viewer.html')) {
+            forwardSelectedOptions();
+            addReloadButtonEventListener();
+        }
+    } catch (error) {
+        console.error('Error during initialization:', error);
+        alert('An error occurred while initialising the application. Please try again.');
     }
 });
