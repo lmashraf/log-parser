451e820780aac76a56d94991b26f813458ea3b9b
Mohammed Achraf Lebrazi <lebrazi.med.achraf@gmail.com>
2024-07-29 22:16:43 +0200

    Some logging

--- renderer.js
+++ renderer.js
@@ -8,18 +8,36 @@ document.addEventListener('DOMContentLoaded', async () => {
     const isMainPage = window.location.pathname.endsWith('index.html') || window.location.pathname === '/';
     const isViewerPage = window.location.pathname.endsWith('viewer.html');
 
+    console.log('DOMContentLoaded event fired');
+
     try {
         if (isMainPage) {
+            console.log('Initializing main page');
             await populateDropdownOptions();
             addRadioEventListeners();
             addMagicButtonEventListener();
         }
 
         if (isViewerPage) {
+            console.log('Initializing viewer page');
             forwardSelectedOptions();
             forwardParsedLogs();
             addReloadButtonEventListener();
         }
+
+        // Conditionally load the Chart for the Viewer
+        // TODO: decouple
+        import('./components/chart.js').then(module => {
+            const Chart = module.default;
+            const chartContainerElement = document.getElementById('chartContainer');
+            if (chartContainerElement) {
+                new Chart('chartContainer');
+            } else {
+                console.error('chartContainer element not found.');
+            }
+        }).catch(error => {
+            console.error('Error loading chart.js:', error);
+        });
     } catch (error) {
         console.error('An error occurred while initialising the application. Please try again.');
         console.error('Error during initialisation:', error);
