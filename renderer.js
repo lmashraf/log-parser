6d72090ebb410d20d8e0ddb10e3c41a635ea6a00
Mohammed Achraf Lebrazi <lebrazi.med.achraf@gmail.com>
2024-07-29 21:51:45 +0200

    Some fixes on chart loading and unnecessary imports

--- renderer.js
+++ renderer.js
@@ -2,8 +2,7 @@ import { populateDropdownOptions } from './renderer/dropdown-options.js';
 import { addRadioEventListeners } from './renderer/radio-events.js';
 import { addMagicButtonEventListener, addReloadButtonEventListener } from './renderer/button-events.js';
 import { forwardSelectedOptions, forwardParsedLogs } from './renderer/process-data.js';
-import './components/tag.js';
-import './components/chart.js';
+
 
 document.addEventListener('DOMContentLoaded', async () => {
     const isMainPage = window.location.pathname.endsWith('index.html') || window.location.pathname === '/';
